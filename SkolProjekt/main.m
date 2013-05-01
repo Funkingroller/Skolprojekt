dd365b80a1b83a7d744b2c7fbb401c325b27ee0e
Björn Dyvelcrona <bjorndyvelcrona@Bjorns-MacBook-Pro.local>
2013-05-01 14:06:37 +0200

    Initial Commit

---
+++ SkolProjekt/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SkolProjekt
+//
+//  Created by Björn Dyvelcrona on 2013-05-01.
+//  Copyright (c) 2013 Björn Dyvelcrona. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
